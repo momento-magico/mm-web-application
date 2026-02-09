f4c7d5646858cf0e1145adebd3668d8064d958e0
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2026-02-09 17:25:54 +0530

    add youtube videos

--- src/app/events/dreamland-2026/components/VideoSection.tsx
+++ src/app/events/dreamland-2026/components/VideoSection.tsx
@@ -1,42 +1,61 @@
 "use client";
 
-import React, { useState } from "react";
-import { Play } from "lucide-react";
+import YouTube from "react-youtube";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+//
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 2000, min: 1000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 2000, min: 1000 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 2000, min: 1000 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 600, min: 0 },
+    items: 1,
+  },
+};
+//
 export function VideoSection() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const handlePlayClick = () => {
-    setIsPlaying(true);
+  const opts = {
+    height: "390",
+    width: "720",
+    playerVars: {
+      autoplay: 1,
+    },
   };
+  const optsMobile = {
+    height: "390",
+    width: "340",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const youtubeIds = ["xHJCy3qKHu0", "omRLu1zHo-k"];
   return (
     <section className="w-full py-10 md:pb-20 px-2 z-50">
       <div className="max-w-5xl mx-auto px-4">
-        <div
-          className="relative rounded-2xl overflow-hidden group cursor-pointer"
-          onClick={handlePlayClick}
-        >
-          {!isPlaying ? (
+        <Carousel responsive={responsive} swipeable draggable={false}>
+          {youtubeIds.map((id) => (
             <>
-              <div className="absolute inset-0 bg-black/40 group-hover:bg-black/50 transition-colors" />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-20 h-20 rounded-full bg-white/10 backdrop-blur-sm flex items-center justify-center border border-white/20 group-hover:scale-110 transition-transform">
-                  <Play className="w-8 h-8 text-white fill-white" />
-                </div>
+              <div key={id} className="w-full md:flex justify-center hidden ">
+                <YouTube videoId={"xHJCy3qKHu0"} opts={opts} />
+              </div>
+              <div key={id} className="w-full flex justify-center md:hidden ">
+                <YouTube videoId={"xHJCy3qKHu0"} opts={optsMobile} />
               </div>
             </>
-          ) : null}
-          <video
-            className="w-full aspect-[16/9] object-cover"
-            poster={"/assets/images/events/thamnail.webp"}
-            controls={isPlaying}
-            playsInline
-          >
-            <source
-              src="https://firebasestorage.googleapis.com/v0/b/thakshara-3c6c3.appspot.com/o/momento%2Fdreamland_intro.mp4?alt=media&token=f901c154-3776-458d-9582-880ffc652db8"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+          ))}
+        </Carousel>
+        <div className=" w-full flex justify-center"></div>
         <h2 className="text-2xl font-medium text-center mt-12">
           The Dream Behind the Beat
         </h2>
